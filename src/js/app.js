2762547d802797010e005647831aa588ee107f3b
Jeff Lau <jeff-lau@live.com>
2016-04-15 14:47:50 +0800

    Added redux counter example

--- src/js/app.js
+++ src/js/app.js
@@ -3,23 +3,53 @@ import "babel-polyfill";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Radium from 'radium';
+import { createStore } from 'redux';
 import "../lib/tests";
 
 // import { Nav } from './components/nav';
 
 
+const store = createStore((state = 0, action)=>{
+  switch(action.type){
+    case "INCREMENT":
+      console.log('incremting in store')
+      return state + 1;
+    break;
+    case "DECREMENT":
+      return state - 1;
+    break;
+    default:
+      return state;
+  }
+})
+
 @Radium
 class App extends React.Component {
   render() {
     return (
-      <div className="app">
-        Hello world, this is a React app
-      </div>
+      <Counter 
+        value={store.getState()} 
+        increment={ () => store.dispatch({type: "INCREMENT"}) }
+        decrement={ () => store.dispatch({type: "DECREMENT"}) }
+      />
     )
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('main')
-);
+const Counter = ({ value, increment, decrement }) => (<div>
+    {value}
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+  </div>
+)
+
+const render = () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('main')
+  );
+}
+
+store.subscribe(render);
+
+render();
